69dcf39c80b6a49c7c9ee926327c64427e261b95
danielbuiles <61318415+danielbuiles@users.noreply.github.com>
2020-09-11 06:39:51 -0500

    create play.php
    el primer trabajo subido a git hub

---
+++ play.php
@@ -0,0 +1,16 @@
+<?php
+
+$nombreUsuario='Ama Marcela Gozo';
+$edadUsuario=37;
+$estatura=8.80;
+$salario=1200;
+$total=$edadUsuario+$salario;
+
+define('PI','3.141516');
+
+echo('Su Total Ganado po Hora es: '.$total);
+
+
+
+
+?>
